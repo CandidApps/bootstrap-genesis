f4bb334e71cc38119f8a411f3177c56c7320e920
Bryan Willis <willis.bryan@live.com>
2015-11-11 13:31:05 -0600

    Change to Plugin

--- functions.php
+++ functions.php
@@ -1,16 +1,30 @@
 <?php
-/**
- * functions.php
- *
- */
+/*
+Plugin Name:        Add Bootstrap to Genesis 
+Plugin URI:         https://github.com/bryanwillis/bootstrap-genesis-addons/
+Description:        Add Bootstrap to Genesis Theme
+Version:            1.0.0
+Author:             bryanwillis
+Author URI:         https://github.com/bryanwillis/
+License:            MIT License
+License URI:        http://opensource.org/licenses/MIT
+*/
 
-/**
- * Include all php files in the /includes directory
- *
- * https://gist.github.com/theandystratton/5924570
- */
-add_action( 'genesis_setup', 'bsg_load_lib_files', 15 );
+
+
 
 function bsg_load_lib_files() {
-    foreach ( glob( dirname( __FILE__ ) . '/lib/*.php' ) as $file ) { include $file; }
+  global $_wp_theme_features;
+  foreach (glob(__DIR__ . '/lib/*.php') as $file) {
+    $feature = 'bsg-' . basename($file, '.php');
+    if (isset($_wp_theme_features[$feature])) {
+      require_once $file;
+    }
+  }
 }
+add_action( 'genesis_setup', 'bsg_load_lib_files', 15 );
+
+
+
+
+
